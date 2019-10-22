66e9f46c54a1692122489a6cf2c8c5a52043bafc
Y.sh <407603405@qq.com>
2019-10-22 11:13:47 +0800

    spring-boot多模块+jpa整合

--- web/src/main/java/com/example/web/WebApplication.java
+++ web/src/main/java/com/example/web/WebApplication.java
@@ -2,8 +2,14 @@ package com.example.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.example")
+@EnableJpaRepositories("com.example.dao")
+@EntityScan("com.example.entity")
 public class WebApplication {
 
     public static void main(String[] args) {
